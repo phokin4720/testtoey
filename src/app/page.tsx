f7c183307e62bfe7f7181d2a49e0d7e0b3d39cc8
Phokin <123359183+phokin4720@users.noreply.github.com>
2025-05-06 11:36:59 +0700

    first comit toey

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Test1</li>
         </ol>
 
         <div className={styles.ctas}>
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
